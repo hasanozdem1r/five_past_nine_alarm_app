13a718f058e2da87c6581166ebf744ced5c24628
Hasan Ozdemir <hozdemir@covidgenomics.com>
2022-06-02 21:47:20 +0300

    When you have don't have mac life is hard

---
+++ five_past_nine/Alarm/GlobalParams.swift
@@ -0,0 +1,18 @@
+//
+//  GlobalParams.swift
+//  Alarm-ios8-swift
+//
+//  Hasan Özdemir
+// 
+
+import Foundation
+
+class Global
+{
+    static var indexOfCell = -1
+    static var isEditMode: Bool = false
+    static var label: String = "Alarm"
+    static var weekdays: [Int] = [Int]()
+    static var mediaLabel: String = "bell"
+    static var snoozeEnabled: Bool = false
+}
